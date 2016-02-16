ae6d352337a862e6eccda5259837d350562aa8fa
Ervand Melikian <melervand@gmail.com>
2016-02-17 00:26:25 +0300

    Added ru_RU localization for Email Validator

---
+++ src/Localization/ru_RU/Email.php
@@ -0,0 +1,11 @@
+<?
+namespace Localization\ru_RU;
+
+use Localization\Localization;
+
+class Email extends Localization {
+    public function message()
+    {
+        return "Неправильный e-mail";
+    }
+}
