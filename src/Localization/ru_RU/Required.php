e91f3e4f9887ab4ff12256cd90423315ae5d3ab3
Ervand Melikian <melervand@gmail.com>
2016-02-29 16:35:07 +0300

    Added "Required" (ru_RU) localization

---
+++ src/Localization/ru_RU/Required.php
@@ -0,0 +1,11 @@
+<?
+namespace Localization\ru_RU;
+
+use Localization\Localization;
+
+class Required extends Localization {
+    public function message()
+    {
+        return "Поле обязательно для заполнения";
+    }
+}
